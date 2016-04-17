22d87f47c801c93540399377f75530f6ac78a786
Alex <alexander.hart@uconn.edu>
2016-04-17 14:44:15 -0400

    Added medications addition layout

--- app/src/main/java/com/alexhart/leukemiaapp/MainActivity.java
+++ app/src/main/java/com/alexhart/leukemiaapp/MainActivity.java
@@ -1,15 +1,42 @@
 package com.alexhart.leukemiaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mMedication, mHydration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mHydration = (Button) findViewById(R.id.Hydration);
+        mMedication = (Button) findViewById(R.id.Medication);
 
+        mHydration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // When button is clicked it will go to mHydration Activity
+//                Intent hydration = new Intent(getBaseContext(), HydrationActivity.class);
+//                startActivity(hydration);
+            }
+        });
+
+        mMedication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //When button is clicked it will go to mMedication Activity
+                Intent medication = new Intent(getBaseContext(), MedicationHolder.class);
+                startActivity(medication);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/alexhart/leukemiaapp/MedicationsFragAdd.java
@@ -0,0 +1,35 @@
+package com.alexhart.leukemiaapp;
+
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MedicationsFragAdd extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_medications_frag_add, container, false);
+
+        FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab_add);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+
+            }
+        });
+        return v;
+
+    }
+
+
+}
